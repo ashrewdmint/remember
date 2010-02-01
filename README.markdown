2e160a5f2554f554219be1e164bc085130ba30ca
Andrew Smith <andrew.caleb.smith@gmail.com>
2010-01-31 17:38:30 -0700

    Why hello there, world!

---
+++ README.markdown
@@ -0,0 +1,55 @@
+# Remember
+
+This jQuery plugin lets you easily track changes made to a form, or any element containing fields.
+
+Where to find this plugin:
+
+*  [jQuery Plugins](http://plugins.jquery.com/project/remember)
+*  [GitHub](http://github.com/ashrewdmint/remember)
+
+## Usage
+
+To use, select any elements containing form fields and call the `remember()` method.
+
+    $('form, .field-container').remember();
+    // Returns any elements selected, so you can call more methods afterwards on the same line.
+
+After changes are made to any fields in the selected elements, you can use `hasChanges`
+to check whether or not changes were made.
+
+    $('form, .field-container').remember('hasChanges');
+    // Returns true or false
+
+Additionally, `changes` returns an array of changes. Each element in the array is an
+object, with `name`, `value`, and `element` properties.
+
+    $('form, .field-container').remember('changes');
+    // Returns something like:
+    // [{name: 'fieldname', value: 'fieldvalue', element: jquery_object}]
+
+Once changes have been made, you may want to use `reset` to reset the old data.
+
+    $('form, .field-container').remember('reset');
+    // Now the hasChanges method will return false
+
+If you are done with this functionality, use the `forget` method.
+
+    $('form, .field-container').remember('forget');
+    // Data associated with this plugin has been forgotten.
+
+And that's all. Enjoy!
+
+## Gotchas
+
+If the plugin does not appear to track changes on a particular field, make sure that the field has
+a name attribute. Remember uses jQuery's built-in `serialize()` method, which only works on fields
+with name attributes.
+
+## License
+
+Copyright (c) 2010 Andrew Smith.
+
+Dual licensed under the MIT and GPL licenses:
+
+*  [http://www.opensource.org/licenses/mit-license.php](http://www.opensource.org/licenses/mit-license.php)
+*  [http://www.gnu.org/licenses/gpl.html](http://www.gnu.org/licenses/gpl.html)
---
+++ remember/jquery.remember.js
@@ -0,0 +1,143 @@
+/*
+ * Remember - jQuery plugin
+ * Easily track changes made to a form, or any element containing fields.
+ *
+ * Copyright (c) 2010 Andrew Smith
+ * Examples and documentation at: http://github.com/ashrewdmint/classnotes
+ * 
+ * Version: 1.0.0 (01/31/2010)
+ * Requires: jQuery v1.3+
+ * 
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+(function($){
+  var remember = {
+    // Key used to set and retrieve data on forms
+    key_name: 'jquery.remember',
+    
+    // Serializes form elements and non-form elements
+    serialize: function(el) {
+      return $('<form></form>').append(el.children().clone()).serialize();
+    },
+    
+    // Finds the last serialization stored in the form. If no data is found,
+    // the form will be serialized and the result stored.
+    last: function(form) {
+      var data = form.data(this.key_name);
+      data = data ? data : this.serialize(form);
+      form.data(this.key_name, data);
+      return data;
+    },
+    
+    // Returns whether or not the form has been changed.
+    hasChanges: function(form) {
+      return this.last(form) != this.serialize(form);
+    },
+    
+    // Returns an array of changes. Each item in the array is an object.
+    // Example result:
+    //   [{name: 'fieldname', value: 'fieldvalue', element: jquery_object}]
+    changes: function(form) {
+      var new_state   = this.params_to_object(this.serialize(form));
+      var saved_state = this.params_to_object(this.last(form));
+      var changes = [];
+      
+      $.each(new_state, function(name, value) {
+        if (saved_state[name] != value) {
+          changes.push({
+            'name':    name,
+            'value':   value,
+            'element': $('[name=' + name + ']')
+          });
+        }
+      });
+      
+      return changes;
+    },
+    
+    // Takes strings returned by form.serialize() and turns them into
+    // objects with keys and values that match the string's keys and values.
+    params_to_object: function(string) {
+      var object = {};
+      var array = string.split('&');
+
+      $.each(array, function(name, value) {
+        value = unescape(value).split('=');
+        object[value[0]] = value[1];
+      });
+
+      return object;
+    },
+    
+    // Erase the remembered form data.
+    forget: function(form) {
+      return form.removeData(this.key_name);
+    }
+  };
+  
+  $.fn.remember = function(method) {
+    var forms, results, result;
+    results = [];
+    
+    if (typeof(method) == 'undefined') {
+      method = 'reset';
+    }
+    
+    this.each(function(index, form) {
+      form = $(form);
+      switch (method) {
+        case 'reset':
+          remember.forget(form);
+          results.push(remember.last(form));
+        break;
+        case 'changes':
+          results.push(remember.changes(form));
+        break;
+        case 'hasChanges':
+          results.push(remember.hasChanges(form));
+        break;
+        case 'forget':
+          results.push(remember.forget(form));
+        break;
+      }
+    });
+    
+    $.each(results, function(index, value) {
+      switch (method) {
+        // Returns array of all changes in all forms
+        case 'changes':
+          if (! result || typeof(result.length) == 'undefined') {
+            result = [];
+          }
+          $.each(value, function(i, change){
+            result.push(change);
+          });
+        break;
+        
+        // Returns true or false depending on whether or not any
+        // changes were made in any of the forms.
+        case 'hasChanges':
+          if (typeof(result) != 'boolean') {
+            result = false;
+          }
+          if (value) {
+            result = true;
+          }
+        break;
+      }
+    });
+    
+    if (method == 'reset' || method == 'forget') {
+      return this;
+    }
+
+    if (typeof(result) == 'undefined') {
+      throw 'Undefined method "' + method + '" for jquery plugin remember';
+    }
+    
+    return result;
+  };
+})(jQuery);
